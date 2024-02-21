7165f7b22bcc1a07756451f828a8e5ee1f09de7b
EdgarSabidoC <a12004194@alumnos.uady.mx>
2024-02-21 10:30:39 -0600

    main_menu script added

---
+++ scenes/main_menu.gd
@@ -0,0 +1,12 @@
+extends Node2D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	# Cambia el tipo de cursor:
+	Input.set_default_cursor_shape(Input.CURSOR_DRAG)
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
