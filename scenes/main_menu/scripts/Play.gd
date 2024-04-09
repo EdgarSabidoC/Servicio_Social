d5f97a731ce03c4ea554cefed1d042e696b4f6f4
EdgarSabidoC <a12004194@alumnos.uady.mx>
2024-04-09 10:06:10 -0600

    Minor changes in Settings.gd

--- scenes/main_menu/scripts/Play.gd
+++ scenes/main_menu/scripts/Play.gd
@@ -3,9 +3,11 @@ extends Button
 #var _move_to: PackedScene
 @export var hint: String = "Comienza a jugar."
 
+
 func _ready() -> void:
 	grab_focus()
 
 
 func _on_pressed() -> void:
+	# Pausa la música global del menú:
 	MenuBackgroundMusic.stop()
