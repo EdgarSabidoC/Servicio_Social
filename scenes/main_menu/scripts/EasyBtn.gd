70df87afebc99d8a91b7bb885e9503ab76d60e0a
EdgarSabidoC <a12004194@alumnos.uady.mx>
2024-06-05 12:10:13 -0600

    Cutscenes scaffold added and small fixes in menus

--- scenes/main_menu/scripts/EasyBtn.gd
+++ scenes/main_menu/scripts/EasyBtn.gd
@@ -3,8 +3,8 @@ extends Button
 
 func _on_pressed():
 	# Prueba para imprimir datos de CharactersData:
-	var cd = get_tree().root.get_node("CharactersData")
-	print(cd.characters[0]["name"]) # Ejemplo de impresión del personaje del nivel 1.
+	CharactersData.loadProblemsData() # Se cargan los datos.
+	print(CharactersData.characters[0]["name"]) # Ejemplo de impresión del personaje del nivel 1.
 	# Consume el evento:
 	get_viewport().set_input_as_handled()
 	# Comienza la animación de desvanecimiento y cambia de escena al final de la animación:
