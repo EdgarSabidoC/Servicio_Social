29b0b030209bec3b763bfb5f34b6a8df850da08c
EdgarSabidoC <63131135+EdgarSabidoC@users.noreply.github.com>
2024-06-08 04:57:47 -0600

    Parallax background added to title_screen scene

--- scenes/title_screen/scripts/TitleScreen.gd
+++ scenes/title_screen/scripts/TitleScreen.gd
@@ -1,17 +1,11 @@
 extends Control
 
-@onready var color_rect: ColorRect = $ColorRect
 @onready var label = $Label
 @export var _move_to: PackedScene
 
 
 func _ready() -> void:
 	assert(_move_to)
-	
-	# Se cargan los datos de los personajes (nombre, bonus, defeated y assets) y se añade el nodo en el árbol:
-	#var LoadData = load("res://scripts/autoloads/data/json_data/CharactersData.gd").new()
-	#LoadData.name = "CharactersData"
-	#get_tree().root.add_child.call_deferred(LoadData)
 
 
 func _process(_delta: float) -> void:
@@ -21,12 +15,10 @@ func _process(_delta: float) -> void:
 		label.speed = 60
 		await get_tree().create_timer(0.5).timeout
 		# Comienza la animación de desvanecimiento y cambia de escena al final de la animación:
-		await fade_out_and_change_scene()
+		change_scene()
 
 
 # Función asíncrona que inicia el desvanecimiento y cambia de escena al final de la animación:
-func fade_out_and_change_scene() -> void:
-	# Reproduce la animación de desvanecimiento del TextureRect
-	await color_rect.fade_out()
+func change_scene() -> void:
 	# Cambia a la nueva escena
 	get_tree().change_scene_to_packed(_move_to)
