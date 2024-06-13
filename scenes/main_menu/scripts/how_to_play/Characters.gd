e8681ecd08e51eb0065815a24256d50326ad8b9c
EdgarSabidoC <a12004194@alumnos.uady.mx>
2024-06-13 14:27:12 -0600

    Some changes on HowToPlay scene (characters tab), but unfinished

--- scenes/main_menu/scripts/how_to_play/Characters.gd
+++ scenes/main_menu/scripts/how_to_play/Characters.gd
@@ -1,10 +1,23 @@
 extends TabBar
 
+@onready var actual_tab: int = 0
+@onready var actual_icon: Texture2D
+@onready var v_box_container: VBoxContainer = $VBoxContainer
+@onready var label: Label = $VBoxContainer/Label
+
+signal pressed
+
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+	v_box_container.hide()
 	if !Mouse.mouse_mode_activated:
 		self.grab_focus()
+
+
+func _process(_delta: float) -> void:
+	if Input.is_action_just_pressed("ui_accept"):
+		pressed.emit()
 
 
 func _on_tab_changed(tab: int) -> void:
@@ -12,11 +25,21 @@ func _on_tab_changed(tab: int) -> void:
 	print_debug("Tab changed %s" % tab)
 
 
-func _on_tab_button_pressed(tab: int) -> void:
-	# Test debug:
-	print_debug("Tab presed %s" % tab)
-
-
 func _on_tab_selected(tab: int) -> void:
 	# Test debug:
 	print_debug("Tab selected %s" % tab)
+	actual_tab = tab
+
+
+func _on_tab_clicked(tab: int) -> void:
+	# Test debug:
+	print_debug("Tab clicked %s" % tab)
+
+
+func _on_pressed() -> void:
+	# Test debug:
+	print_debug("Tab pressed %s" % actual_tab)
+	for tab in tab_count:
+		if actual_tab != tab:
+			set_tab_hidden(tab, true)
+	v_box_container.show()
