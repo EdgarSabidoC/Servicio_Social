550598f8d0c7e987be6fee60c1ac827e9fd027eb
EdgarSabidoC <63131135+EdgarSabidoC@users.noreply.github.com>
2024-06-17 16:26:23 -0600

    Ticket and Total label animations added to LevelAdditionsMinigame scene

--- scenes/additions_minigame/level_additions_minigame/scripts/AcceptBtn.gd
+++ scenes/additions_minigame/level_additions_minigame/scripts/AcceptBtn.gd
@@ -1,9 +1,19 @@
 extends Button
 
-@onready var ticket_animation_sprite: AnimatedSprite2D = $"../../TicketAnimationSprite"
+@onready var total_label: Label = $"../../TicketTexture/TotalLabel"
+@onready var total_animation_player: AnimationPlayer = $"../../TicketTexture/TotalLabel/TotalAnimationPlayer"
+@onready var ticket_texture: TextureRect = $"../../TicketTexture"
+@onready var ticket_animation_player: AnimationPlayer = $"../../TicketTexture/TicketAnimationPlayer"
 
+@onready var active: bool = false
 
 func _on_pressed() -> void:
-	ticket_animation_sprite.show()
-	ticket_animation_sprite.animation = "default"
-	ticket_animation_sprite.play()
+	change_active_state()
+	
+	if self.active:
+		ticket_texture.show()
+		ticket_animation_player.play("default")
+
+
+func change_active_state(): 
+	self.active = !self.active
--- scenes/additions_minigame/level_additions_minigame/scripts/ClearBtn.gd
+++ scenes/additions_minigame/level_additions_minigame/scripts/ClearBtn.gd
@@ -1,5 +1,10 @@
 extends Button
 
+@onready var accept_btn: Button = $"../AcceptBtn"
+@onready var total_label: Label = $"../../TicketTexture/TotalLabel"
+@onready var ticket_texture: TextureRect = $"../../TicketTexture"
+
 
 func _on_pressed() -> void:
-	pass # Replace with function body.
+	ticket_texture.hide()
+	accept_btn.change_active_state()
