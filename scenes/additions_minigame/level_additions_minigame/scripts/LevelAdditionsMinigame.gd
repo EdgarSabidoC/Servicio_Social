f480a057ddda37e0cca9ef1aa90e9a292655f1dd
EdgarSabidoC <a12004194@alumnos.uady.mx>
2024-06-18 11:05:31 -0600

    Score, clock and pause menu added to LevelAdditionsMinigame

--- scenes/additions_minigame/level_additions_minigame/scripts/LevelAdditionsMinigame.gd
+++ scenes/additions_minigame/level_additions_minigame/scripts/LevelAdditionsMinigame.gd
@@ -17,12 +17,23 @@ extends Node2D
 @onready var accept_btn: Button = $CanvasLayer/VBoxContainer/AcceptBtn
 @onready var clear_btn: Button = $CanvasLayer/VBoxContainer/ClearBtn
 @onready var prices_btn: Button = $CanvasLayer/PricesBtn
+@onready var pause: Control = $CanvasLayer/Pause
+@onready var clock: Clock = $CanvasLayer/Clock
+@onready var score_label: Label = $CanvasLayer/ScorePanel/ScoreLabel
+@onready var prices_menu: Control = $CanvasLayer/PricesMenu
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	# Se inicializa el puntaje en 0:
 	PlayerSession.score = 0
+	
+	# Se imprime el puntaje:
+	self.score_label.print_score()
+	
+	# Se activan los controles de pausa:
+	if !self.pause.is_pausable_scene:
+		self.pause.is_pausable_scene = true
 	#if !Mouse.mouse_mode_activated:
 		#disable_buttons()
 	#else:
@@ -31,9 +42,9 @@ func _ready() -> void:
 	
 
 func _process(_delta: float) -> void:
-	if total_label.text.length() > 12:
+	if self.total_label.text.length() > 12:
 		disable_buttons()
-	elif !buttons_are_enabled:
+	elif !self.buttons_are_enabled:
 		enable_buttons()
 	
 	if Input.is_action_just_pressed("num_0"):
@@ -58,72 +69,104 @@ func _process(_delta: float) -> void:
 		self.total_label.text += "9"
 	elif Input.is_action_just_pressed("period"):
 		if !self.total_label.text.contains("."):
-			if self.total_label.text.begins_with(".")
+			if self.total_label.text.begins_with("."):
 				print_debug(self.total_label.text)
 			else:
 				self.total_label.text += "."
-
-func _input(_event: InputEvent) -> void:
-	pass
+	elif Input.is_action_just_pressed("ui_pause"):
+		if !self.pause.is_active():
+			self.clock.stop()
+			self.pause.show()
+	
 
 
 func disable_buttons() -> void:
-	buttons_are_enabled = false
-	for button in buttons:
-		button.disabled = true
+	self.buttons_are_enabled = false
+	for button in self.buttons:
+		self.button.disabled = true
 
 
 func enable_buttons() -> void:
-	if buttons_are_enabled:
-		for button in buttons:
-			button.disabled = false
-			buttons_are_enabled = true
+	if self.buttons_are_enabled:
+		for button in self.buttons:
+			self.button.disabled = false
+			self.buttons_are_enabled = true
 
 
 func _on_button_dot_pressed() -> void:
-	if !total_label.text.contains("."):
-		total_label.text += "."
+	if !self.total_label.text.contains("."):
+		self.total_label.text += "."
 
 
 func _on_button_0_pressed() -> void:
-	total_label.text += "0"
+	self.total_label.text += "0"
 
 
 func _on_button_1_pressed() -> void:
-	total_label.text += "1"
+	self.total_label.text += "1"
 
 
 func _on_button_2_pressed() -> void:
-	total_label.text += "2"
+	self.total_label.text += "2"
 
 
 func _on_button_3_pressed() -> void:
-	total_label.text += "3"
+	self.total_label.text += "3"
 
 
 func _on_button_4_pressed() -> void:
-	total_label.text += "4"
+	self.total_label.text += "4"
 
 
 func _on_button_5_pressed() -> void:
-	total_label.text += "5"
+	self.total_label.text += "5"
 
 
 func _on_button_6_pressed() -> void:
-	total_label.text += "6"
+	self.total_label.text += "6"
 
 
 func _on_button_7_pressed() -> void:
-	total_label.text += "7"
+	self.total_label.text += "7"
 
 
 func _on_button_8_pressed() -> void:
-	total_label.text += "8"
+	self.total_label.text += "8"
 
 
 func _on_button_9_pressed() -> void:
-	total_label.text += "9"
+	self.total_label.text += "9"
 
 
 func _on_button_clear_pressed() -> void:
-	total_label.text = total_label.text.left(-1)
+	self.total_label.text = self.total_label.text.left(-1)
+
+
+func _on_pause_finished() -> void:
+	if !Mouse.mouse_mode_activated:
+		self.answer_button_1.grab_focus()
+	self.clock.continue_clock()
+	self.pause.hide()
+
+
+func _on_pause_btn_pressed() -> void:
+	if !self.pause.is_active():
+			self.clock.stop()
+			self.pause.show()
+
+
+# Cuando se llegue a un minuto nuevo se aumenta la velocidad de la música:
+func _on_clock_new_minute_reached() -> void:
+	# Se aumenta el pitch_scale de la música por cada minuto de juego:
+	self.current_pitch += 0.1
+	BackgroundMusic.change_pitch(self.current_pitch)
+
+
+func _on_prices_btn_pressed() -> void:
+	self.prices_menu.show()
+	self.clock.stop()
+
+
+func _on_prices_menu_back_btn_pressed() -> void:
+	self.prices_menu.hide()
+	self.clock.continue_clock()
