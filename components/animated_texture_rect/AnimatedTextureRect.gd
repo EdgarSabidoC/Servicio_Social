94b02687fdff4ff576360a83552bfce26309c43e
EdgarSabidoC <63131135+EdgarSabidoC@users.noreply.github.com>
2024-06-20 12:47:05 -0600

    New AnimatedTextureRect created and Alux scene main node changed to AnimatedTextureRect

---
+++ components/animated_texture_rect/AnimatedTextureRect.gd
@@ -0,0 +1,83 @@
+@tool
+class_name AnimatedTextureRect extends TextureRect
+@export var sprites: SpriteFrames
+@export_enum("default", "sad", "anger") var current_animation: String
+@export var frame_index: int = 0
+@export_range(0.0, INF, 0.001) var speed_scale: float = 1.0
+@export var auto_play: bool = false
+@export var playing: bool = false
+@onready var refresh_rate: float = 1.0
+@onready var fps: float = 30
+@onready var frame_delta: float = 0
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	self.fps = self.sprites.get_animation_speed(self.current_animation)
+	self.refresh_rate = self.sprites.get_frame_duration(self.current_animation, self.frame_index)
+	if self.auto_play:
+		self.play()
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	if self.sprites == null or self.playing == false:
+		return
+	elif self.sprites.has_animation(self.current_animation) == false:
+		self.playing = false
+		assert(false, "Animation %s, does not exist" % current_animation)
+	
+	self.get_animation_data(self.current_animation)
+	self.frame_delta += (self.speed_scale * delta)
+	if self.frame_delta >= self.refresh_rate/self.fps:
+		self.texture = self.get_next_frame()
+		self.frame_delta = 0
+	
+
+# Reproduce la animación:
+func play(animation: String = current_animation) -> void:
+	self.frame_index = 0
+	self.frame_delta = 0
+	self.current_animation = animation
+	self.get_animation_data(self.current_animation)
+	self.playing = true
+
+
+# Obtiene los datos de la animación:
+func get_animation_data(animation: String) -> void:
+	self.fps = self.sprites.get_animation_speed(self.animation)
+	self.refresh_rate = self.sprites.get_frame_duration(self.animation, self.frame_index)
+
+
+# Obtiene el siguiente frame de la animación:
+func get_next_frame() -> Texture2D:
+	# Se aumenta el índice de frames:
+	self.frame_index += 1
+	# Se obtiene la cuenta total de frames:
+	var frame_count = self.sprites.get_frame_count(self.current_animation)
+	# Se reincia el índice de los frames si se llega a un valor mayor o igual al total de frames:
+	if self.frame_index >= frame_count:
+		self.frame_index = 0
+		# Se verifica si no se está en un loop:
+		if not self.sprites.get_animation_loop(self.current_animation):
+			self.playing = false
+	get_animation_data(self.current_animation)
+	
+	# Se retorna la siguiente textura de la animación:
+	return self.sprites.get_frame_texture(self.current_animation, self.frame_index)
+
+
+# Continúa con la animación si estaba pausada previamente:
+func resume() -> void:
+	self.playing = true
+
+
+# Pausa la animación:
+func pause() -> void:
+	self.playing = false
+
+
+# Para la animación:
+func stop() -> void:
+	self.frame_index = 0
+	self.playing = false
