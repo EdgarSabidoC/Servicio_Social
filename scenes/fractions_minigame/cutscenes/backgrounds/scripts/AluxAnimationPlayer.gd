890b1d4f244eda18dbdb521d2cfc6e482ea561cc
EdgarSabidoC <63131135+EdgarSabidoC@users.noreply.github.com>
2024-06-20 22:00:03 -0600

    Signals for characters manually connected in Scene01

--- scenes/fractions_minigame/cutscenes/backgrounds/scripts/AluxAnimationPlayer.gd
+++ scenes/fractions_minigame/cutscenes/backgrounds/scripts/AluxAnimationPlayer.gd
@@ -5,14 +5,13 @@ extends AnimationPlayer
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	self.alux.connect("finished", _on_alux_finished)
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta: float) -> void:
 	if alux.is_visible_in_tree() and self.alux.current_animation == "entrance" and self.alux.frame_index == 30:
-		#self.play("entrance")
-		pass
+		self.play("entrance")
 
 
 func _on_alux_finished() -> void:
--- scenes/fractions_minigame/cutscenes/backgrounds/scripts/ZotzAnimationPlayer.gd
+++ scenes/fractions_minigame/cutscenes/backgrounds/scripts/ZotzAnimationPlayer.gd
@@ -5,14 +5,13 @@ extends AnimationPlayer
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	self.zotz.connect("finished", _on_zotz_finished)
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta: float) -> void:
 	if zotz.is_visible_in_tree() and self.zotz.current_animation == "entrance" and self.zotz.frame_index == 30:
-		#self.play("entrance")
-		pass
+		self.play("entrance")
 
 
 func _on_zotz_finished() -> void:
