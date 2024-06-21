aad89c7e2d2c4a170185f351e297cda83922ad02
EdgarSabidoC <a12004194@alumnos.uady.mx>
2024-06-21 12:52:34 -0600

    Small changes on characters animations for Scene01

--- scenes/fractions_minigame/cutscenes/backgrounds/scripts/KekenAnimationPlayer.gd
+++ scenes/fractions_minigame/cutscenes/backgrounds/scripts/KekenAnimationPlayer.gd
@@ -10,7 +10,7 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta: float) -> void:
-	if self.keken.current_animation == "entrance" and self.keken.frame_index == 10:
+	if self.keken.current_animation == "entrance" and self.keken.frame_index == 9:
 		self.play("entrance")
 
 
