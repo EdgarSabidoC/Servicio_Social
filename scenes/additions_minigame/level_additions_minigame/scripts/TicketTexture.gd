64ef8478d4069c4472df99b292fc16b59be1cb86
EdgarSabidoC <a12004194@alumnos.uady.mx>
2024-06-27 11:02:31 -0600

    Changes on LevelAdditionsMinigame: TicketAnimationPlayer small fixes

--- scenes/additions_minigame/level_additions_minigame/scripts/TicketTexture.gd
+++ scenes/additions_minigame/level_additions_minigame/scripts/TicketTexture.gd
@@ -1,7 +1,6 @@
 extends TextureRect
 
 @onready var total_number_label: Label = $"../TotalNumberLabel"
-@onready var total_animation_player: AnimationPlayer = $TicketTotalLabel/TotalAnimationPlayer
 @onready var ticket_animation_player: AnimationPlayer = $TicketAnimationPlayer
 @onready var ticket_total_label: Label = $TicketTotalLabel
 @onready var total_label: Label = $"../TotalLabel"
@@ -9,19 +8,11 @@ extends TextureRect
 
 func _on_visibility_changed() -> void:
 	if !self.is_visible_in_tree():
-		ticket_animation_player.play("RESET")
-		total_animation_player.play("RESET")
+		ticket_animation_player.play("reset")
+		ticket_total_label.text = "TOTAL:\n\n"
 
 
-func _on_animation_player_animation_finished(_anim_name: StringName) -> void:
-	print_debug("Finalizó la animación del ticket")
-
-
-func _on_animation_player_animation_started(_anim_name: StringName) -> void:
-	total_animation_player.play("default")
-
-
-func _on_total_animation_player_animation_finished(anim_name: StringName) -> void:
+func _on_animation_player_animation_finished(anim_name: StringName) -> void:
 	if anim_name == "default":
 		if self.total_label.text.ends_with("."):
 			ticket_total_label.text += "$%s00" % total_label.text
