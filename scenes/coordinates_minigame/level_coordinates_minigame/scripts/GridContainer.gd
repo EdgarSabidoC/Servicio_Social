c1b40d131d9fd9785c6136b16d63e2e3b2da34b8
EdgarSabidoC <63131135+EdgarSabidoC@users.noreply.github.com>
2024-07-09 12:33:40 -0600

    Small changes on autoloads

--- scenes/coordinates_minigame/level_coordinates_minigame/scripts/GridContainer.gd
+++ scenes/coordinates_minigame/level_coordinates_minigame/scripts/GridContainer.gd
@@ -1,7 +1,7 @@
 extends GridContainer
 
 @onready var drag_texture: DragTexture = $"../DragTexture"
-@onready var drop_slot: DropSlot = $DropSlot
+
 
 
 func _on_drop_slot_texture_dropped(coordinates: Vector2i) -> void:
