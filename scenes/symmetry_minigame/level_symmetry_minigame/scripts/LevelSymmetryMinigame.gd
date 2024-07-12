bfb5f9810c4b568462e128fe04901f8fadb0d421
EdgarSabidoC <63131135+EdgarSabidoC@users.noreply.github.com>
2024-07-12 14:43:46 -0600

    S4L and S4R are now symmetric

--- scenes/symmetry_minigame/level_symmetry_minigame/scripts/LevelSymmetryMinigame.gd
+++ scenes/symmetry_minigame/level_symmetry_minigame/scripts/LevelSymmetryMinigame.gd
@@ -13,24 +13,37 @@ extends Node2D
 @export var time_medium: float = 120
 @export var time_hard: float = 90
 
-# Lista de rebanadas:
-@onready var slice_list: Array[TextureRect] = [%Slice1, %Slice2, %Slice3, %Slice4]
-
 # Lista de ingredientes (lado izquierdo):
-@onready var ingredient_list: Array[AnimatedTextureRect] = [\
-	%S1L1, %S1L2, %S1L3, %S1L4, \
-	%S2L1, %S2L2, %S2L3, \
-	%S3L1, %S3L2, %S3L3, %S3L4, \
-	%S4L1, %S4L2, %S4L3, %S4L4, \
+@onready var ingredient_list: Array = [\
+	[%S1L1, %S1L2, %S1L3, %S1L4], \
+	[%S2L1, %S2L2, %S2L3], \
+	[%S3L1, %S3L2, %S3L3, %S3L4], \
+	[%S4L1, %S4L2, %S4L3, %S4L4], \
 ]
 
 # Lista de ranuras (lado derecho):
-@onready var drop_slot_list: Array[AnimatedTextureRect] = [\
-	%S1R1, %S1R2, %S1R3, %S1R4, \
-	%S2R1, %S2R2, %S2R3, \
-	%S3R1, %S3R2, %S3R3, %S3R4, \
-	%S4R1, %S4R2, %S4R3, %S4R4, \
+@onready var drop_slot_list: Array = [\
+	[%S1R1, %S1R2, %S1R3, %S1R4], \
+	[%S2R1, %S2R2, %S2R3], \
+	[%S3R1, %S3R2, %S3R3, %S3R4], \
+	[%S4R1, %S4R2, %S4R3, %S4R4], \
 ]
+
+# Lista de rebanadas izquierdas:
+@onready var left_slice_list: Dictionary = {
+	"s1": [%S1L, ingredient_list[0]], \
+	"s2": [%S2L, ingredient_list[1]], \
+	"s3": [%S3L, ingredient_list[2]], \
+	"s4": [%S4L, ingredient_list[3]], \
+}
+
+# Lista de rebanadas derechas:
+@onready var right_slice_list: Dictionary = {
+	"s1": [%S1R, drop_slot_list[0]], \
+	"s2": [%S2R, drop_slot_list[1]], \
+	"s3": [%S3R, drop_slot_list[2]], \
+	"s4": [%S4R, drop_slot_list[3]], \
+}
 
 
 func _enter_tree() -> void:
@@ -69,10 +82,33 @@ func set_clock() -> void:
 			self.clock.time = self.time_hard
 
 
+# Configura las rebanadas que se mostrarán:
+func set_pizza() -> void:
+	var rng = RandomNumberGenerator.new()
+	rng.randomize()
+	
+	# Lista de claves (s1, s2, s3, s4)
+	var slice_keys = left_slice_list.keys()
+	
+	# Ocultar hasta un máximo de 3 rebanadas de pizza de manera aleatoria
+	var slices_to_hide = min(randi_range(1,3), slice_keys.size())
+	for slice in range(slices_to_hide):
+		var index = rng.randi_range(0, slice_keys.size() - 1)
+		var key = slice_keys[index]
+		left_slice_list[key][0].hide()  # Oculta la rebanada izquierda
+		right_slice_list[key][0].hide() # Oculta la rebanada derecha
+		for ingredient in left_slice_list[key][1]:
+			ingredient.hide()  # Oculta los ingredientes de la izquierda
+		for ingredient in right_slice_list[key][1]:
+			ingredient.hide()  # Oculta las ranuras de la derecha
+		slice_keys.remove_at(index)
+
+
 func set_ingredients():
 	# Se generan los ingredientes aleatorios:
-	for ingredient in self.ingredient_list:
-		ingredient.generate_rand_ingredient()
+	for list in self.ingredient_list:
+		for ingredient in list:
+			ingredient.generate_rand_ingredient()
 
 
 # Configura la partida:
@@ -85,6 +121,7 @@ func set_game() -> void:
 	
 	# Se configuran el tiempo y los ingredientes:
 	self.set_clock()
+	self.set_pizza()
 	self.set_ingredients()
 	
 	# Contiúa el reloj:
@@ -103,9 +140,10 @@ func _on_pause_finished() -> void:
 
 
 func _on_reset_pressed() -> void:
-	for slot in self.drop_slot_list:
-		if slot.texture:
-			slot.clear_data()
+	for list in self.drop_slot_list:
+		for slot in list:
+			if slot.texture:
+				slot.clear_data()
 
 
 func _on_pause_btn_pressed() -> void:
