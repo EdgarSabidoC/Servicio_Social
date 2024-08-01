e317018a89b2acfdf99c38a7c25d01caa8b7a173
EdgarSabidoC <63131135+EdgarSabidoC@users.noreply.github.com>
2024-07-31 19:03:48 -0600

    ScreteLevel removed from outro cutscene

--- scenes/fractions_minigame/cutscenes/outro_cutscene/scripts/OutroCutscene.gd
+++ scenes/fractions_minigame/cutscenes/outro_cutscene/scripts/OutroCutscene.gd
@@ -24,11 +24,6 @@ func _input(event: InputEvent) -> void:
 		if PlayerSession.character < 5:
 			# Se carga un nuevo nivel:
 			SceneTransition.change_scene(self.intro_cutscene)
-		elif PlayerSession.secret_level:
-			# Se desactiva el nivel secreto:
-			PlayerSession.secret_level = false
-			var secret_cutscene = load("res://scenes/fractions_minigame/cutscenes/secret_level/SecretCutscene.tscn")
-			SceneTransition.change_scene(secret_cutscene) # Se cambia al nivel secreto
 		else:
 			# Se finaliza el juego cambiando a la escena final:
 			SceneTransition.change_scene(self.end_cutscene)
