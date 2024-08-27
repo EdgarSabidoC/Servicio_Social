8da1fbee18fea6c0ab05a1dd3517cf57b52cced0
EdgarSabidoC <a12004194@alumnos.uady.mx>
2024-08-27 11:06:08 -0600

    Hints for InfoScreen added on MainMenu

--- scenes/info_screen/scripts/InfoScreen.gd
+++ scenes/info_screen/scripts/InfoScreen.gd
@@ -3,10 +3,11 @@ extends Control
 @onready var animated_sprite_2d: AnimatedSprite2D = $AnimatedSprite2D
 @onready var texture_rect: TextureRect = $TextureRect
 @onready var dialogue_box: Control = $DialogueBox
-@export_multiline var fractions_text: String = ""
-@export_multiline var additions_text: String = ""
-@export_multiline var coordinates_text: String = ""
-@export_multiline var symmetry_text: String = ""
+@export_multiline var fractions_text: String = "[StartParagraph]El juego consiste en encontrar la fracción correcta de pizza correspondiente al problema.\n\nEn dificultad media y difícil también es necesario hallar la proporción correcta de bebidas y órdenes de pan.
+[StartParagraph]Completa las órdenes en el menor tiempo para conseguir más puntos."
+@export_multiline var additions_text: String = "[StartParagraph]El juego consiste en realizar la suma correcta presentada en la nota de venta."
+@export_multiline var coordinates_text: String = "[StartParagraph]El juego consiste en repartir las pizzas en la mesa con la coordenada correspondiente."
+@export_multiline var symmetry_text: String = "[StartParagraph]El juego consiste en colocar los ingredientes correspondientes en el lado derecho y rotarlos para hacer que la pizza quede simétrica con respecto al eje Y."
 
 
 ## Finished signal is emitted when the info screen finishes.
@@ -24,12 +25,24 @@ func start() -> void:
 			PlayerSession.fractions_info_screen = true
 			self.dialogue_box.load_message(self.fractions_text)
 		PlayerSession.Minigames.ADDITIONS:
+			if PlayerSession.is_practice_mode():
+				self.additions_text += "[StartParagraph]En el modo práctica no se adquieren puntos.\n\n[b]Modo contrareloj desactivado.[/b]"
+			else: 
+				self.additions_text += "[StartParagraph][b]Contrareloj activado:[/b] Mientras más tiempo pase, menos puntos obtendrás, así que sé lo más rápido que puedas."
 			PlayerSession.additions_info_screen = true
 			self.dialogue_box.load_message(self.additions_text)
 		PlayerSession.Minigames.COORDINATES:
+			if PlayerSession.is_practice_mode():
+				self.coordinates_text += "[StartParagraph]En el modo práctica no se adquieren puntos.\n\n[b]Modo contrareloj desactivado.[/b]"
+			else: 
+				self.coordinates_text += "[StartParagraph][b]Contrareloj activado:[/b] Mientras más tiempo pase, menos puntos obtendrás, así que sé lo más rápido que puedas."
 			PlayerSession.coordinates_info_screen = true
 			self.dialogue_box.load_message(self.coordinates_text)
 		PlayerSession.Minigames.SYMMETRY:
+			if PlayerSession.is_practice_mode():
+				self.symmetry_text += "[StartParagraph]En el modo práctica no se adquieren puntos.\n\n[b]Modo contrareloj desactivado.[/b]"
+			else: 
+				self.symmetry_text += "[StartParagraph][b]Contrareloj activado:[/b] Mientras más tiempo pase, menos puntos obtendrás, así que sé lo más rápido que puedas."
 			PlayerSession.symmetry_info_screen = true
 			self.dialogue_box.load_message(self.symmetry_text)
 	self.dialogue_box.start()
