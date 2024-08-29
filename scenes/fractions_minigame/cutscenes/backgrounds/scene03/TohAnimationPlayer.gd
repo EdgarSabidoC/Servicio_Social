d2f6addef01c3cb0e02cd0c3d94326c82d30ecea
carlosruiz01 <CARD_@hotmail.com>
2024-08-29 11:50:38 -0600

    scene 03 outros animations added to all characters

--- scenes/fractions_minigame/cutscenes/backgrounds/scene03/TohAnimationPlayer.gd
+++ scenes/fractions_minigame/cutscenes/backgrounds/scene03/TohAnimationPlayer.gd
@@ -1,37 +1,37 @@
 extends AnimationPlayer
 
-@onready var zotz: AnimatedTextureRect = $".."
+@onready var toh: AnimatedTextureRect = $".."
 @onready var current_character: CharacterResource
 @onready var dialogue_box: Control = $"../../DialogueBox"
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	self.zotz.connect("finished", self._on_zotz_finished)
+	self.toh.connect("finished", self._on_toh_finished)
 	# Se obtiene de la lista de personajes el actual:
 	self.current_character = CharactersData.characters[PlayerSession.character]
 	self.dialogue_box.connect("dialogue_box_closed", self.start_exit)
 
 
-func _on_zotz_finished() -> void:
-	self.zotz.changed = true
+func _on_toh_finished() -> void:
+	self.toh.changed = true
 	# Una vez termina la animación default, se cambia:
 	if self.current_character.defeated:
-		self.zotz.enable_loop()
+		self.toh.enable_loop()
 	elif self.current_character.rejected:
 		# Si el personaje fue rechazado:
-		self.zotz.current_animation = "sad"
+		self.toh.current_animation = "sad"
 		# Se desactiva el loop de la animación:
-		self.zotz.disable_loop()
+		self.toh.disable_loop()
 	else:
 		# Si no:
-		self.zotz.current_animation = "angry"
+		self.toh.current_animation = "angry"
 		# Se desactiva el loop de la animación:
-		self.zotz.disable_loop()
+		self.toh.disable_loop()
 	# Se desconecta la señal:
-	self.zotz.disconnect("finished", self._on_zotz_finished)
+	self.toh.disconnect("finished", self._on_toh_finished)
 	# Se reproduce la animación:
-	self.zotz.playing = true
+	self.toh.playing = true
 
 
 func start_exit() -> void:
